35de3f0b74d6992f94b781f3de5272561be731e4
Rohan Nayan <rohannayan405@gmail.com>
2023-05-06 15:36:06 +0530

    Mongoose-Encryption

---
+++ app.js
@@ -0,0 +1,65 @@
+//jshint esversion:6
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const mongoose = require('mongoose');
+const encrypt = require('mongoose-encryption');
+const connection = mongoose.createConnection("mongodb+srv://rohannayan405:qw36Yes1P5QmwBYT@cluster0.1qehh9x.mongodb.net/usersDB");
+
+const app = express();
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
+});
+
+//Mongoose-Encryption
+const secretKey = "This is a Secret";
+userSchema.plugin(encrypt, {secret: secretKey, encryptedFields: ['password']});
+
+const User = connection.model("User", userSchema);
+
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/", function(req, res){
+    res.render("home");
+});
+app.get("/login", function(req, res){
+    res.render("login");
+});
+app.get("/register", function(req, res){
+    res.render("register");
+});
+
+app.post("/register", function(req,res){
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    })
+    try{
+        newUser.save();
+        res.render("secrets");
+    }catch(err){
+        console.log(err);
+    };
+});
+
+app.post("/login", function(req, res){
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({email:username}).then(user=>{
+        if(user.password==password){
+            res.render("secrets");
+        }
+        else{
+            res.send("Invalid Credentials");
+        }
+    })
+});
+
+app.listen(3000, function(){
+    console.log("Server started on port 3000");
+})
